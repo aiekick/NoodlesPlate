7515368a23d362877db37f30aad51dc57a06f09b
aiekick <aiekick@funparadigm.com>
2023-10-08 19:53:40 +0200

    -

--- src/Headers/CustomInAppGpuProfiler.h
+++ src/Headers/CustomInAppGpuProfiler.h
@@ -22,68 +22,51 @@
 // without that you will have many linker error
 #include <GLFW/glfw3.h>
 
-// #define GPU_CONTEXT void*
-#define GPU_CONTEXT GLFWwindow*
+// #define IAGP_GPU_CONTEXT void*
+#define IAGP_GPU_CONTEXT GLFWwindow*
 
-//#define GET_CURRENT_CONTEXT GetCurrentContext
-inline GPU_CONTEXT GetCurrentContext() {
+//#define IAGP_GET_CURRENT_CONTEXT GetCurrentContext
+inline IAGP_GPU_CONTEXT GetCurrentContext() {
     return glfwGetCurrentContext();
 }
-#define GET_CURRENT_CONTEXT GetCurrentContext
+#define IAGP_GET_CURRENT_CONTEXT GetCurrentContext
 
-// #define SET_CURRENT_CONTEXT SetCurrentContext
-inline void SetCurrentContext(GPU_CONTEXT vContext) {
+// #define IAGP_SET_CURRENT_CONTEXT SetCurrentContext
+inline void SetCurrentContext(IAGP_GPU_CONTEXT vContext) {
     glfwMakeContextCurrent(vContext);
 }
-#define SET_CURRENT_CONTEXT SetCurrentContext
+#define IAGP_SET_CURRENT_CONTEXT SetCurrentContext
 
 ////////////////////////////////////////////////////////////////
 // OPTIONNAL ///////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
 // the title of the profiler detail imgui windows
-#define AIGP_DETAILS_TITLE "Profiler Details"
+#define IAGP_DETAILS_TITLE "Profiler Details"
 
 // the max level of recursion for profiler queries
-#define RECURSIVE_LEVELS_COUNT 20U
+#define IAGP_RECURSIVE_LEVELS_COUNT 20U
 
 // the mean average level
 // all the values will be smoothed on 60 frames (1s of 60fps diosplay)
-#define MEAN_AVERAGE_LEVELS_COUNT 60U
+#define IAGP_MEAN_AVERAGE_LEVELS_COUNT 60U
 
 // the minimal size of imgui sub window, when you openif by click right on a progiler bar
-#define SUB_AIGP_WINDOW_MIN_SIZE ImVec2(500, 120)
+#define IAGP_SUB_WINDOW_MIN_SIZE ImVec2(500, 120)
 
 // the imgui button to use in IAGP
 #define IAGP_IMGUI_BUTTON ImGui::ContrastedButton
 
 // the Imgui Play/Pause button to use
-// #define IMGUI_PLAY_PAUSE_BUTTON
-#define ICON_NDP_PLAY u8"\uffd4"
-#define ICON_NDP_PAUSE u8"\uffd2"
-inline bool PlayPauseButton(bool& vIsPaused) {
-    bool res = false;
-    const char* play_pause_label = ICON_NDP_PAUSE;
-    if (vIsPaused) {
-        play_pause_label = ICON_NDP_PLAY;
-    }
-    if (ImGui::ContrastedButton(play_pause_label)) {
-        vIsPaused = !vIsPaused;
-        res = true;
-    }
-    if (ImGui::IsItemHovered()) {
-        ImGui::SetTooltip("Play/Pause Profiling");
-    }
-    return res;
-}
-#define IMGUI_PLAY_PAUSE_BUTTON PlayPauseButton
+#define IAGP_IMGUI_PLAY_LABEL u8"\uffd4"
+#define IAGP_IMGUI_PAUSE_LABEL u8"\uffd2"
 
 // define your fucntion for log error message of IAGP
 // #define LOG_ERROR_MESSAGE LogError
 #include <ctools/Logger.h>
-#define LOG_ERROR_MESSAGE LogVarError
+#define IAGP_LOG_ERROR_MESSAGE LogVarError
 
 // define your fucntion for log error message of IAGP only in debug
 // #define LOG_DEBUG_ERROR_MESSAGE LogDebugError
-#define LOG_DEBUG_ERROR_MESSAGE LogVarDebugError
+#define IAGP_LOG_DEBUG_ERROR_MESSAGE LogVarDebugError
 
